12b63becfb08db59f2e6605951d02fb8ece83d31
Руслан Аббасов <103284052+RuslanAbbasov@users.noreply.github.com>
2023-01-07 13:39:28 +0500

    Add files via upload

---
+++ src/Model.java
@@ -0,0 +1,17 @@
+public class Model {
+    public int id;
+    public int district;
+    public String school;
+    public String county;
+    public String grades;
+    public int students;
+    public float teachers;
+    public float calworks;
+    public float lunch;
+    public int computer;
+    public float expenditure;
+    public float income;
+    public float english;
+    public float read;
+    public float math;
+}
